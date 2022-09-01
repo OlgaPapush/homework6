4808ef6f482a6d4ef6d09909b46e0514d90535c8
Ольга <koval.oi.1106@gmail.com>
2022-09-01 08:27:10 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // 1 задание
+        int [] array1 = new int [3];
+        array1 [0] =  1;
+        array1 [1] = 2;
+        array1 [3] = 3;
+        for ( int i = 0; i < array1.length ; i++) {
+            System.out.println ( array1 [i]);
+        }
+        double [] array2 = { 1.57, 7.654,9.986};
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println ( array2 [i]); }
+
+            String[] array3 = { " Массив", "номер", "три"};
+            for (int i = 0; i < array3.length; i ++) {
+                System.out.println ( array3 [i]);
+            }
+
+
     }
 }
