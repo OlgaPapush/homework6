61399cbfa0cb11fa1930f1e6385b66dfb7a76f34
Ольга <koval.oi.1106@gmail.com>
2022-09-01 09:09:21 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,41 @@ public class Main {
     public static void main(String[] args) {
 
         // 1 задание
-        int [] array1 = new int [3];
-        array1 [0] =  1;
-        array1 [1] = 2;
-        array1 [3] = 3;
-        for ( int i = 0; i < array1.length ; i++) {
-            System.out.println ( array1 [i]);
+        int [] arr1 = new int [3];
+        arr1 [0] =  1;
+        arr1 [1] = 2;
+        arr1 [3] = 3;
+        for ( int i = 0; i < arr1.length ; i++) {
+            System.out.println ( arr1 [i]);
         }
-        double [] array2 = { 1.57, 7.654,9.986};
-        for (int i = 0; i < array2.length; i++) {
-            System.out.println ( array2 [i]); }
+        double [] arr2 = { 1.57, 7.654,9.986};
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println ( arr2 [i]); }
 
-            String[] array3 = { " Массив", "номер", "три"};
-            for (int i = 0; i < array3.length; i ++) {
-                System.out.println ( array3 [i]);
+            String[] arr3 = { " Массив", "номер", "три"};
+            for (int i = 0; i < arr3.length; i ++) {
+                System.out.println ( arr3 [i]);
             }
 
+            // 2 задание
 
+            int [] array1 ;
+            arr1 = new int [3];
+            arr1 [0] = 1;
+        arr1 [0] = 2;
+        arr1 [0] = 3;
+        System.out.println ( arr1 [0] + ",");
+        System.out.println ( arr1 [1] + ",");
+        System.out.println ( arr1 [2] + ",");
+
+        double[] array2 = { 1.57,7.654,9.986};
+        System.out.println ( arr2 [0] + ",");
+        System.out.println ( arr2 [1] + ",");
+        System.out.println ( arr2 [2] + ",");
+
+        String[] array3 = {"Массив", "номер", "три"};
+        System.out.println ( arr3 [0] + ",");
+        System.out.println ( arr3 [1] + ",");
+        System.out.println ( arr3 [2] + ",");
     }
 }
