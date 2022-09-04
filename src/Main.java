a44360963908d8168e033da76f688110bc89b9b7
Ольга <koval.oi.1106@gmail.com>
2022-09-04 13:48:14 +0300

    дз на проверку

--- src/Main.java
+++ src/Main.java
@@ -2,62 +2,61 @@ public class Main {
     public static void main(String[] args) {
 
         // 1 задание
-        int [] arr1 = new int [3];
-        arr1 [0] =  1;
-        arr1 [1] = 2;
-        arr1 [3] = 3;
-        for ( int i = 0; i < arr1.length ; i++) {
-            System.out.println ( arr1 [i]);
+        int [] arr = new int [3];
+        arr [0] =  1;
+        arr [1] = 2;
+        arr [2] = 3;
+        for ( int i = 0; i < arr.length ; i++) {
+            System.out.println ( arr [i]);
         }
         double [] arr2 = { 1.57, 7.654,9.986};
         for (int i = 0; i < arr2.length; i++) {
             System.out.println ( arr2 [i]); }
 
-            String[] arr3 = { " Массив", "номер", "три"};
+            long [] arr3 = { 2020, 2021, 2022 };
             for (int i = 0; i < arr3.length; i ++) {
                 System.out.println ( arr3 [i]);
             }
 
-            // 2 задание
+        // 2 задание
 
-            int [] array1 ;
-            arr1 = new int [3];
-            arr1 [0] = 1;
-        arr1 [0] = 2;
-        arr1 [0] = 3;
-        System.out.println ( arr1 [0] + ",");
-        System.out.println ( arr1 [1] + ",");
-        System.out.println ( arr1 [2] + ",");
+        int [] array = new int [3] ;
+        array [0] = 1;
+        array [1] = 2;
+        array [2] = 3;
+        System.out.println ( array [0] + ",");
+        System.out.println ( array [1] + ",");
+        System.out.println ( array [2] + ",");
 
         double[] array2 = { 1.57,7.654,9.986};
-        System.out.println ( arr2 [0] + ",");
-        System.out.println ( arr2 [1] + ",");
-        System.out.println ( arr2 [2] + ",");
+        System.out.println ( array2 [0] + ",");
+        System.out.println ( array2 [1] + ",");
+        System.out.println ( array2 [2] + ",");
 
-        String[] array3 = {"Массив", "номер", "три"};
-        System.out.println ( arr3 [0] + ",");
-        System.out.println ( arr3 [1] + ",");
-        System.out.println ( arr3 [2] + ",");
+       long [] array3 = { 2020, 2021, 2022 };
+        System.out.println ( array3 [0] + ",");
+        System.out.println ( array3 [1] + ",");
+        System.out.println ( array3 [2] + ",");
 
-        // 3 задание
+// 3 задание
 
-        int [] arr1 = new int [3];
-        arr1 [0] = 1;
-        arr1 [1] = 2;
-        arr1 [2] = 3;
-        System.out.println ( arr1 [2] + ",");
-        System.out.println ( arr1 [1] + ",");
-        System.out.println ( arr1 [0] + ",");
+        int [] arr11 = new int [3];
+        arr11 [0] = 1;
+        arr11 [1] = 2;
+        arr11 [2] = 3;
+        System.out.println ( arr11 [2] + ",");
+        System.out.println ( arr11 [1] + ",");
+        System.out.println ( arr11 [0] + ",");
 
-        double[] arr2 = { 1.57,7.654,9.986};
-        System.out.println ( arr2 [2] + ",");
-        System.out.println ( arr2 [1] + ",");
-        System.out.println ( arr2 [0] + ",");
+        double[] arr22 = { 1.57,7.654,9.986 };
+        System.out.println ( arr22 [2] + ",");
+        System.out.println ( arr22 [1] + ",");
+        System.out.println ( arr22 [0] + ",");
 
-        String[] arr3 = {"Массив", "номер", "три"};
-        System.out.println ( arr3 [2] + ",");
-        System.out.println ( arr3 [1] + ",");
-        System.out.println ( arr3 [1] + ",");
+        long [] arr33 = { 2020,2021,2022 };
+        System.out.println ( arr33 [2] + ",");
+        System.out.println ( arr33 [1] + ",");
+        System.out.println ( arr33 [1] + ",");
 
         // 4 задание
 
