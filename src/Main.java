97136344d5f13015e1b63d17b301b07e9b52ce88
Ольга <koval.oi.1106@gmail.com>
2022-09-08 07:02:58 +0300

    исправила 2-3 задание

--- src/Main.java
+++ src/Main.java
@@ -24,19 +24,18 @@ public class Main {
         array [0] = 1;
         array [1] = 2;
         array [2] = 3;
-        System.out.println ( array [0] + ",");
-        System.out.println ( array [1] + ",");
-        System.out.println ( array [2] + ",");
+        for ( int i = 0; i < arr.length ; i++) {
+            System.out.println(arr[i] + " , ");
+        }
 
-        double[] array2 = { 1.57,7.654,9.986};
-        System.out.println ( array2 [0] + ",");
-        System.out.println ( array2 [1] + ",");
-        System.out.println ( array2 [2] + ",");
+        double[] array2 = { 1.57, 7.654,9.986};
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println ( arr2 [i] + ","); }
 
-       long [] array3 = { 2020, 2021, 2022 };
-        System.out.println ( array3 [0] + ",");
-        System.out.println ( array3 [1] + ",");
-        System.out.println ( array3 [2] + ",");
+        long [] array3 = { 2020, 2021, 2022 };
+        for (int i = 0; i < arr3.length; i ++) {
+            System.out.println ( arr3 [i] + ",");
+        }
 
 // 3 задание
 
@@ -44,19 +43,18 @@ public class Main {
         arr11 [0] = 1;
         arr11 [1] = 2;
         arr11 [2] = 3;
-        System.out.println ( arr11 [2] + ",");
-        System.out.println ( arr11 [1] + ",");
-        System.out.println ( arr11 [0] + ",");
+        for ( int i = 0; i > arr11.length ; i--) {
+            System.out.println(arr[i] + " , ");
+        }
 
-        double[] arr22 = { 1.57,7.654,9.986 };
-        System.out.println ( arr22 [2] + ",");
-        System.out.println ( arr22 [1] + ",");
-        System.out.println ( arr22 [0] + ",");
+        double[] arr22 = { 1.57, 7.654,9.986};
+        for (int i = 0; i > arr2.length; i--) {
+            System.out.println ( arr2 [i] + ","); }
 
-        long [] arr33 = { 2020,2021,2022 };
-        System.out.println ( arr33 [2] + ",");
-        System.out.println ( arr33 [1] + ",");
-        System.out.println ( arr33 [1] + ",");
+        long [] arr33 = { 2020, 2021, 2022 };
+        for (int i = 0; i > arr3.length; i --) {
+            System.out.println ( arr3 [i] + ",");
+        }
 
         // 4 задание
 
